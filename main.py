9945eb863e8fe4945dcabb3925fed3e8d92579c3
Alba Lopes <78361086+profaalbalopes@users.noreply.github.com>
2022-06-15 22:51:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cardapio')
+def cardapio():
+    return render_template('cardapio.html', promocao=False)
+
+@app.route('/avaliacoes')
+def avaliacoes():
+  clientes = [
+    {'nome': 'Maria', 'nota': 5},
+    {'nome': 'José', 'nota': 3},
+    {'nome': 'Pedro', 'nota': 4},
+    {'nome': 'Bianca', 'nota': 3},
+    {'nome': 'Lorena', 'nota': 2},  
+  ]
+  return render_template('avaliacoes.html', clientes=clientes)
+
+@app.route('/faleconosco')
+def faleconosco():
+    return render_template('faleconosco.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+app.run(host='0.0.0.0', port=81)
